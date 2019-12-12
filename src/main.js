37eae9b623ed13dfdb1ad9d50902dc1785a39e27
su-git123 <57624602+su-git123@users.noreply.github.com>
2019-12-12 16:54:05 +0800

    初始化-整合路由

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
 
+// 导入 Vue
+import Vue from 'vue'
+// 导入 根组件
+import App from '@/App.vue'
+// 导入 路由
+import router from '@/router'
+
+//是否显示生产提示
+Vue.config.productionTip = false
+
+// 创建Vue示例
+new Vue({
+  render: h => h(App),
+  // 挂载到Vue实例上
+  router
+})
+// 挂载到id为app的div上
+.$mount('#app')
 Vue.config.productionTip = false
 
 new Vue({
